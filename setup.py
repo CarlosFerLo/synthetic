7b79e8f190a8d6fa56a160e1671c660ab2af71e6
Carlos Fernandez <carlos.ferlo@outlook.com>
2023-09-27 12:32:15 +0200

    comment out version change for production for tests

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from distutils.core import setup
 from setuptools import find_packages
-import os
-
 
 setup(
     name="synthetic-cognition",
-    version="{{VERSION_PLACEHOLDER}}" if os.environ["ENV"] == "PROD" else "0.0.1",
+    version="0.0.1",
     description="Synthetic Cognition general inference architecture for language model agents DEMO",
     packages=find_packages("."),
     long_description="Synthetic Cognition general inference architecture for language model agents DEMO",
