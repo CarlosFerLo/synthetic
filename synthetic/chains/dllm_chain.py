f8d93ed77bef4f49ac3242ef5fa3bc2e8b5b29fe
Carlos Fernandez <carlos.ferlo@outlook.com>
2023-09-27 15:16:01 +0200

    add verification of format to dllm chain

--- synthetic/chains/dllm_chain.py
+++ synthetic/chains/dllm_chain.py
@@ -1,6 +1,7 @@
 from typing import Any
 from langchain.prompts import PromptTemplate
 
+from synthetic import dstr, pdstr
 from synthetic.llms import DynamicLLM
 
 class DynamicLLMChain () :
@@ -8,9 +9,19 @@ class DynamicLLMChain () :
     prompt_template: PromptTemplate
     
     def __init__(self, dllm: DynamicLLM, prompt_template: PromptTemplate) -> None:
+        try :
+            pdstr(prompt_template.template)
+        except :
+            raise ValueError("PromptTemplate can not be converted to pdstr.")
+        
         self.dllm = dllm
         self.prompt_template = prompt_template
         
         
     def format(self, **kwargs: Any) -> str :
         return self.prompt_template.format(**kwargs)
+    
+    def run(self, **kwargs: Any) -> dstr :
+        prompt = self.format(**kwargs)
+        pdstring=pdstr(prompt)
+        return self.dllm(pdstring)
--- test/test_dllm_chain.py
+++ test/test_dllm_chain.py
@@ -3,12 +3,13 @@ import unittest
 from langchain.llms.fake import FakeListLLM
 from langchain.prompts import PromptTemplate
 
+from synthetic import dstr
 from synthetic.llms import DynamicLLM
 from synthetic.chains import DynamicLLMChain
 
 class DynamicLLMChainTest (unittest.TestCase):
     def test_dllm_chain_can_be_created_from_dllm_and_prompt_template (self):
-        prompt_template = PromptTemplate(input_variables=["input"], template="{input}")
+        prompt_template = PromptTemplate(input_variables=["input"], template="<HEAD>{input}</HEAD>")
         llm = FakeListLLM(responses=[""])
         dllm = DynamicLLM(llm=llm)
         
@@ -17,17 +18,24 @@ class DynamicLLMChainTest (unittest.TestCase):
         self.assertIsInstance(chain, DynamicLLMChain)
         
     def test_dllm_chain_format_method_accepts_string_string_pairs_and_outputs_final_prompt (self):
-        prompt_template = PromptTemplate(input_variables=["input", "output"], template="{input} -> {output}")
+        prompt_template = PromptTemplate(input_variables=["input", "output"], template="<HEAD>{input}</HEAD><START>{output}<END>")
         llm = FakeListLLM(responses=[""])
         dllm = DynamicLLM(llm=llm)
         chain = DynamicLLMChain(dllm, prompt_template)
         
         out = chain.format(input="input", output="output")
         self.assertIsInstance(out, str)
-        self.assertEqual(out, "input -> output")
+        self.assertEqual(out, "<HEAD>input</HEAD><START>output<END>")
+        
+    def test_dllm_chain_fails_to_init_if_prompt_template_template_is_not_a_valid_pdstr (self) :
+        prompt_template = PromptTemplate(input_variables=["input"], template="{input}</HEAD>")
+        llm = FakeListLLM(responses=[""])
+        dllm = DynamicLLM(llm=llm)
+        
+        self.assertRaises(ValueError, DynamicLLMChain, prompt_template, dllm)
         
     def test_dllm_chain_format_method_fails_if_one_input_var_is_missing (self) :
-        prompt_template = PromptTemplate(input_variables=["input", "output"], template="{input} -> {output}")
+        prompt_template = PromptTemplate(input_variables=["input", "output"], template="<HEAD>{input}</HEAD><START>{output}<END>")
         llm = FakeListLLM(responses=[""])
         dllm = DynamicLLM(llm=llm)
         chain = DynamicLLMChain(dllm, prompt_template)
@@ -36,10 +44,33 @@ class DynamicLLMChainTest (unittest.TestCase):
             chain.format(input="input")
             
     def test_dllm_chain_format_method_fails_if_one_extra_parameter_is_passed (self):
-        prompt_template = PromptTemplate(input_variables=["input"], template="{input}")
+        prompt_template = PromptTemplate(input_variables=["input"], template="<HEAD>{input}</HEAD>")
         llm = FakeListLLM(responses=[""])
         dllm = DynamicLLM(llm=llm)
         chain = DynamicLLMChain(dllm, prompt_template)
         
         with self.assertRaises(KeyError) :
             chain.format(input="input", output="output")
+            
+    def test_dllm_chain_can_be_run_by_passing_correct_kwargs_and_returns_dstr (self):
+        prompt_template = PromptTemplate(input_variables=["input"], template="<HEAD>{input}</HEAD><START>")
+        llm = FakeListLLM(responses=["output<END>"])
+        dllm = DynamicLLM(llm=llm)
+        chain = DynamicLLMChain(dllm, prompt_template)
+        
+        output = chain.run(input = "input")
+        
+        self.assertIsInstance(output, dstr)
+        self.assertEqual(output.raw, "<HEAD>input</HEAD><START>output<END>")
+        
+    def test_dllm_chain_fails_if_format_fails (self):
+        prompt_template = PromptTemplate(input_variables=["input"], template="<HEAD>{input}</HEAD><START>")
+        llm = FakeListLLM(responses=["output<END>"])
+        dllm = DynamicLLM(llm=llm)
+        chain = DynamicLLMChain(dllm, prompt_template)
+        
+        with self.assertRaises(KeyError) :
+            chain.run()
+        with self.assertRaises(KeyError) :
+            chain.run(input="input", output="output")
+    
